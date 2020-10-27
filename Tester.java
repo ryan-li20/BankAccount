fba128d83b4b1019505dd0a5deb6a074fb269aa6
ryan-li20 <rli20@stuy.edu>
2020-10-27 17:30:06 -0400

    my tester

--- Tester.java
+++ Tester.java
@@ -2,22 +2,22 @@ public class Tester{
   public static void main(String[] args){
     BankAccount first = new BankAccount(513234, "myPassword");
     System.out.println(first.getAccountID());
-    System.out.println(first.getBalance(513234));
+    System.out.println(first.getBalance());
     System.out.println(first.toString());
     System.out.println(first.getPassword());
     first.setPassword("minePass");
     System.out.println(first.getPassword());
     System.out.println(first.deposit(10));
-    System.out.println(first.getBalance(513234));
+    System.out.println(first.getBalance());
     System.out.println(first.deposit(-14));
-    System.out.println(first.getBalance(513234));
+    System.out.println(first.getBalance());
     System.out.println();
     System.out.println(first.withdraw(9));
-    System.out.println(first.getBalance(513234));
+    System.out.println(first.getBalance());
     System.out.println(first.withdraw(-2));
-    System.out.println(first.getBalance(513234));
+    System.out.println(first.getBalance());
     System.out.println(first.withdraw(3));
-    System.out.println(first.getBalance(513234));
-
+    System.out.println(first.getBalance());
+    System.out.println(first.toString());
   }
 }
